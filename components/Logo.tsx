0576d0714985b37872ef3716d2665ab0b0f72e7e
vyasvishal <vyasvishal106.work@gmail.com>
2024-12-20 13:22:42 +0530

    last commit

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <Link href="/" passHref>
-      <span className="font-bold text-3xl bg-gradient-to-bl from-gray-50 to-gray-300 text-transparent bg-clip-text cursor-pointer">
+      <span className="font-bold text-3xl bg-gradient-to-b from-white to-gray-300 text-transparent bg-clip-text cursor-pointer">
         FormLab
       </span>
     </Link>
